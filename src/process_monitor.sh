305cda5132794b565be0b95e841e8bca8d2c4fb1
robin3910 <rob.luobin@gmail.com>
2024-05-30 22:52:56 +0800

    update: monitor server

--- src/process_monitor.sh
+++ src/process_monitor.sh
@@ -3,7 +3,7 @@
 while true 
 do
 	monitor=`ps -ef | grep node | grep -v "auto" | wc -l ` 
-	if [ $monitor -eq 0 ] 
+	if [ $monitor -eq 1 ]
 	then
 		echo "Manipulator program is not running, restart Manipulator"
 		nohup node server.js > node.out &
